b92687466c7a0d593aeb46cd23129edee7efdde8
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-10-01 10:50:40 +0530

    Create FibonacciRecursion.java
    Write a Java program to find the Fibonacci numbers using recursion.

---
+++ Infoane_Technologies/FibonacciRecursion.java
@@ -0,0 +1,10 @@
+public class FibonacciRecursion{
+  public static void main(String ... abhinna){
+      int a=0,b=1,c=0;
+      for(int i=0;i<10;i++){
+        c=a+b;
+        a=b;b=c;
+        System.out.println(c);
+      }
+  }
+}
